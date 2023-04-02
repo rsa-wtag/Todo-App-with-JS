1dbdf3ea809465548d9a48af25d51e5820286e6a
Rumman Sadik <rumman.sadik@welldev.io>
2023-04-02 13:39:55 +0600

    Added code formatter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,48 @@
-window.addEventListener('load', () => {
-    const form = document.querySelector('#create-task')
-    const inputTask = document.querySelector('#input-task')
-    const taskList = document.querySelector('#tasks')
-    const createBtn = document.querySelector('#create')
-    const cancel = document.querySelector('#cancel')
+window.addEventListener("load", () => {
+  const form = document.querySelector("#create-task");
+  const inputTask = document.querySelector("#input-task");
+  const taskList = document.querySelector("#tasks");
+  const createBtn = document.querySelector("#create");
+  const cancel = document.querySelector("#cancel");
 
-    createBtn.addEventListener('click', () => {
-        if (form.className == "hide"){
-            form.classList.remove("hide")
-        }
-        inputTask.focus()
-    })
+  createBtn.addEventListener("click", () => {
+    if (form.className == "hide") {
+      form.classList.remove("hide");
+    }
+    inputTask.focus();
+  });
 
-    cancel.addEventListener('click', () => {
-        form.classList.add("hide")
-        inputTask.value = ""
-    })
+  cancel.addEventListener("click", () => {
+    form.classList.add("hide");
+    inputTask.value = "";
+  });
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        if (!inputTask.value){
-            return
-        }
+    if (!inputTask.value) {
+      return;
+    }
 
-        const task =document.createElement('div')
-        task.classList.add("task")
+    const task = document.createElement("div");
+    task.classList.add("task");
 
-        const task_content =document.createElement('div')
-        task_content.classList.add("content")
+    const task_content = document.createElement("div");
+    task_content.classList.add("content");
 
-        const currentDate = new Date();
-        const options = { day: '2-digit', month: '2-digit', year: '2-digit' };
-        const formattedDate = currentDate.toLocaleDateString('en-UK', options).replace(/\//g, '.');
+    const currentDate = new Date();
+    const options = { day: "2-digit", month: "2-digit", year: "2-digit" };
+    const formattedDate = currentDate
+      .toLocaleDateString("en-UK", options)
+      .replace(/\//g, ".");
 
-        task_content.innerText = inputTask.value + "\nCreated At: " +  formattedDate
+    task_content.innerText = inputTask.value + "\nCreated At: " + formattedDate;
 
-        task.appendChild(task_content)
+    task.appendChild(task_content);
 
-        taskList.prepend(task)
+    taskList.prepend(task);
 
-        inputTask.value = ""
-        form.classList.add("hide")
-    })
-})
+    inputTask.value = "";
+    form.classList.add("hide");
+  });
+});
