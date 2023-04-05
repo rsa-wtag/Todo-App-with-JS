00803e4ac015c09b87f12f1efb47654d0a85f176
Rumman Sadik <rumman.sadik@welldev.io>
2023-04-05 11:18:28 +0600

    added constants.js file

--- assets/js/add-task.js
+++ assets/js/add-task.js
@@ -1,10 +1,11 @@
+import {CLICK_EVENT} from "../js/constants.js";
+
 window.addEventListener("load", () => {
   const form = document.querySelector("#create-task");
   const inputValue = document.querySelector("#input-value");
   const taskList = document.querySelector("#task-list");
   const createButton = document.querySelector("#create-button");
   const cancelButton = document.querySelector("#cancel-button");
-  const CLICK_EVENT = "click";
 
   createButton.addEventListener(CLICK_EVENT, () => {
     if (form.className == "hide") {
