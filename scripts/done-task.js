4aa7a8696c50fe059eb25b87b33256bf64a08b48
Rumman Sadik <rumman.sadik@welldev.io>
2023-04-16 15:42:27 +0600

    extracted a method to calculate the diffDays outside the handler.

--- scripts/done-task.js
+++ scripts/done-task.js
@@ -11,17 +11,20 @@ function onTaskComplete(
 ) {
   doneButton.addEventListener(CLICK_EVENT, doneTask);
 
+  function getTaskCompletedDays(time) {
+    const now = Date.now();
+    const diffTime = now - time;
+    const diffHours = diffTime / (1000 * 60 * 60);
+    return Math.ceil(diffHours / 24);
+  }
+
   function doneTask() {
     inputElement.classList.add("done");
     const task = tasks[id];
     task.doneButton = true;
     doneButton.classList.add(HIDE_CLASS);
     editButton.classList.add(HIDE_CLASS);
-
-    const now = Date.now();
-    const diffTime = now - task.date.getTime();
-    const diffHours = diffTime / (1000 * 60 * 60);
-    const diffDays = Math.ceil(diffHours / 24);
+    const diffDays = getTaskCompletedDays(task.date.getTime());
 
     const completedText =
       diffDays === 1 ? `Completed in 1 day` : `Completed in ${diffDays} days`;
