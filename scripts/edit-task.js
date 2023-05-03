e921de63b90d49b29a314ec338e9791118d8b504
Rumman Sadik <rumman.sadik@welldev.io>
2023-05-03 11:54:29 +0600

    divided the functions

--- scripts/edit-task.js
+++ scripts/edit-task.js
@@ -2,6 +2,174 @@ import { CLICK_EVENT, HIDE_CLASS } from "/scripts/constants.js";
 import createButton from "/scripts/factory/createButton.js";
 import toggleButtons from "/scripts/factory/toggleButton.js";
 
+function saveEvent(
+  doneButton,
+  editButton,
+  deleteButton,
+  saveAndDoneButton,
+  revertButton,
+  saveButton,
+  inputElement,
+  tasks,
+  id
+) {
+  inputElement.contentEditable = false;
+  tasks[id].content = inputElement.textContent;
+  toggleButtons(
+    editButton,
+    deleteButton,
+    revertButton,
+    saveButton,
+    doneButton,
+    saveAndDoneButton
+  );
+  console.log("call");
+  saveButton.removeEventListener(CLICK_EVENT, saveEvent);
+  revertButton.removeEventListener(CLICK_EVENT,  revertEvent);
+  saveAndDoneButton.removeEventListener(CLICK_EVENT, onTodoDone);
+}
+
+function onTodoDone(
+  doneButton,
+  editButton,
+  deleteButton,
+  saveAndDoneButton,
+  revertButton,
+  saveButton,
+  inputElement,
+  toolbar,
+  tasks,
+  id
+) {
+  inputElement.contentEditable = false;
+  inputElement.classList.add("done");
+  const task = tasks[id];
+  task.done = true;
+  task.content = inputElement.textContent;
+
+  editButton.removeEventListener(CLICK_EVENT, onEditButtonClick);
+  doneButton.removeEventListener(CLICK_EVENT, onTodoDone);
+
+  toggleButtons(saveAndDoneButton, saveButton, revertButton, deleteButton);
+
+  const diffDays = getTaskCompletedDays(task.date.getTime());
+  const completedText =
+    diffDays === 1 ? `Completed in 1 day` : `Completed in ${diffDays} days`;
+  const completeTimeBtn = createButton(
+    "complete-time",
+    completedText,
+    "Time to complete the task"
+  );
+  task.completeTime = diffDays;
+  toolbar.append(completeTimeBtn);
+
+  saveButton.removeEventListener(CLICK_EVENT, saveEvent);
+  revertButton.removeEventListener(CLICK_EVENT, revertEvent);
+  saveAndDoneButton.removeEventListener(CLICK_EVENT, onTodoDone);
+}
+
+function revertEvent(
+  doneButton,
+  editButton,
+  deleteButton,
+  saveAndDoneButton,
+  revertButton,
+  saveButton,
+  inputElement,
+  tasks,
+  id,
+  prevContent
+) {
+  inputElement.innerText = prevContent;
+  inputElement.contentEditable = false;
+  tasks[id].content = inputElement.textContent;
+  toggleButtons(
+    editButton,
+    deleteButton,
+    revertButton,
+    saveButton,
+    doneButton,
+    saveAndDoneButton
+  );
+  saveButton.removeEventListener(CLICK_EVENT, saveEvent);
+  revertButton.removeEventListener(CLICK_EVENT, revertEvent);
+  saveAndDoneButton.removeEventListener(CLICK_EVENT, onTodoDone);
+}
+
+function getTaskCompletedDays(time) {
+  const now = Date.now();
+  const diffTime = now - time;
+  const diffHours = diffTime / (1000 * 60 * 60);
+  return Math.ceil(diffHours / 24);
+}
+
+function onEditButtonClick(
+  doneButton,
+  editButton,
+  deleteButton,
+  saveAndDoneButton,
+  revertButton,
+  saveButton,
+  inputElement,
+  toolbar,
+  tasks,
+  id
+) {
+  const prevContent = inputElement.innerText;
+  inputElement.contentEditable = true;
+  inputElement.focus();
+  toggleButtons(
+    editButton,
+    deleteButton,
+    revertButton,
+    saveButton,
+    saveAndDoneButton,
+    doneButton
+  );
+
+  saveButton.addEventListener(CLICK_EVENT, () =>
+    saveEvent(
+      doneButton,
+      editButton,
+      deleteButton,
+      saveAndDoneButton,
+      revertButton,
+      saveButton,
+      inputElement,
+      tasks,
+      id
+    )
+  );
+  saveAndDoneButton.addEventListener(CLICK_EVENT, () =>
+    onTodoDone(
+      doneButton,
+      editButton,
+      deleteButton,
+      saveAndDoneButton,
+      revertButton,
+      saveButton,
+      inputElement,
+      toolbar,
+      tasks,
+      id
+    )
+  );
+  revertButton.addEventListener(CLICK_EVENT, () =>
+    revertEvent(
+      doneButton,
+      editButton,
+      deleteButton,
+      saveAndDoneButton,
+      revertButton,
+      saveButton,
+      inputElement,
+      tasks,
+      id,
+      prevContent
+    )
+  );
+}
+
 function onTaskEdit(
   doneButton,
   editButton,
@@ -14,93 +182,20 @@ function onTaskEdit(
   tasks,
   id
 ) {
-  function onEditButtonClick() {
-    const prevContent = inputElement.innerText;
-    inputElement.contentEditable = true;
-    inputElement.focus();
-    toggleButtons(
+  editButton.addEventListener(CLICK_EVENT, () =>
+    onEditButtonClick(
+      doneButton,
       editButton,
       deleteButton,
+      saveAndDoneButton,
       revertButton,
       saveButton,
-      saveAndDoneButton,
-      doneButton
-    );
-
-    function saveEvent() {
-      inputElement.contentEditable = false;
-      tasks[id].content = inputElement.textContent;
-      toggleButtons(
-        editButton,
-        deleteButton,
-        revertButton,
-        saveButton,
-        doneButton,
-        saveAndDoneButton
-      );
-      saveButton.removeEventListener(CLICK_EVENT, saveEvent);
-      revertButton.removeEventListener(CLICK_EVENT, revertEvent);
-      saveAndDoneButton.removeEventListener(CLICK_EVENT, onTodoDone);
-    }
-
-    function onTodoDone() {
-      inputElement.contentEditable = false;
-      inputElement.classList.add("done");
-      const task = tasks[id];
-      task.done = true;
-      task.content = inputElement.textContent;
-
-      editButton.removeEventListener(CLICK_EVENT, onEditButtonClick);
-      doneButton.removeEventListener(CLICK_EVENT, onTodoDone);
-
-      toggleButtons(saveAndDoneButton, saveButton, revertButton, deleteButton);
-
-      const diffDays = getTaskCompletedDays(task.date.getTime());
-      const completedText =
-        diffDays === 1 ? `Completed in 1 day` : `Completed in ${diffDays} days`;
-      const completeTimeBtn = createButton(
-        "complete-time",
-        completedText,
-        "Time to complete the task"
-      );
-      task.completeTime = diffDays;
-      toolbar.append(completeTimeBtn);
-
-      saveButton.removeEventListener(CLICK_EVENT, saveEvent);
-      revertButton.removeEventListener(CLICK_EVENT, revertEvent);
-      saveAndDoneButton.removeEventListener(CLICK_EVENT, onTodoDone);
-    }
-
-    function getTaskCompletedDays(time) {
-      const now = Date.now();
-      const diffTime = now - time;
-      const diffHours = diffTime / (1000 * 60 * 60);
-      return Math.ceil(diffHours / 24);
-    }
-
-    function revertEvent() {
-      inputElement.innerText = prevContent;
-      inputElement.contentEditable = false;
-      tasks[id].content = inputElement.textContent;
-      toggleButtons(
-        editButton,
-        deleteButton,
-        revertButton,
-        saveButton,
-        doneButton,
-        saveAndDoneButton
-      );
-      saveButton.removeEventListener(CLICK_EVENT, saveEvent);
-      revertButton.removeEventListener(CLICK_EVENT, revertEvent);
-      saveAndDoneButton.removeEventListener(CLICK_EVENT, onTodoDone);
-    }
-
-    saveButton.addEventListener(CLICK_EVENT, saveEvent);
-    saveAndDoneButton.addEventListener(CLICK_EVENT, onTodoDone);
-    revertButton.addEventListener(CLICK_EVENT, revertEvent);
-  }
-
-  editButton.addEventListener(CLICK_EVENT, onEditButtonClick);
+      inputElement,
+      toolbar,
+      tasks,
+      id
+    )
+  );
 }
 
 export default onTaskEdit;
